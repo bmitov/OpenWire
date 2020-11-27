790642195d0aa8f0b1ca65ba06a75f0397a6808b
bmitov <mitov@mitov.com>
2020-11-27 11:32:23 -0800

    Property editors updated to the latest Mitov.Design API

--- OpenWire.Delphi.PropertyEditors.pas
+++ OpenWire.Delphi.PropertyEditors.pas
@@ -3,7 +3,7 @@
 //     This software is supplied under the terms of a license agreement or    //
 //     nondisclosure agreement with Mitov Software and may not be copied      //
 //     or disclosed except in accordance with the terms of that agreement.    //
-//         Copyright(c) 2002-2016 Mitov Software. All Rights Reserved.        //
+//         Copyright(c) 2002-2020 Mitov Software. All Rights Reserved.        //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -13,7 +13,7 @@ interface
 
 uses
   System.Classes, System.Types, Vcl.Graphics, DesignIntf, VCLEditors,
-  Mitov.Design, OpenWirePinEditors, Mitov.TypeInfo;
+  Mitov.Design, OpenWirePinEditors, Mitov.TypeInfo, Mitov.Containers.List;
 
 type
   TOWPinListOwnerPropertyEditorProxy = class( OpenWirePinEditors.TOWPinListOwnerPropertyEditor, IInterface, IProperty, IPropertyKind, IProperty70, IInheritedPropertyEditor,
@@ -28,34 +28,34 @@ type
    FOwnerTypeInfo : ITypeInfo; // For chache speedup!
 
   protected
-    function QueryInterface(const IID: TGUID; out Obj): HResult; stdcall;
+    function QueryInterface( const IID: TGUID; out Obj ): HResult; stdcall;
 
   protected
-    procedure PropDrawName(ACanvas: TCanvas; const ARect: TRect; ASelected: Boolean);
-    procedure PropDrawValue(ACanvas: TCanvas; const ARect: TRect; ASelected: Boolean);
+    procedure PropDrawName( ACanvas : TCanvas; const ARect : TRect; ASelected : Boolean );
+    procedure PropDrawValue( ACanvas : TCanvas; const ARect : TRect; ASelected : Boolean );
 
-    function  PropDrawNameRect(const ARect: TRect): TRect;
-    function  PropDrawValueRect(const ARect: TRect): TRect;
+    function  PropDrawNameRect( const ARect : TRect ): TRect;
+    function  PropDrawValueRect( const ARect : TRect ): TRect;
 
   protected
-    procedure ListMeasureWidth(const Value: String; ACanvas: TCanvas; var AWidth: Integer);
-    procedure ListMeasureHeight(const Value: String; ACanvas: TCanvas; var AHeight: Integer);
-    procedure ListDrawValue(const Value: String; ACanvas: TCanvas; const ARect: TRect; ASelected: Boolean);
+    procedure ListMeasureWidth( const AValue : String; ACanvas : TCanvas; var AWidth : Integer );
+    procedure ListMeasureHeight( const AValue : String; ACanvas : TCanvas; var AHeight : Integer );
+    procedure ListDrawValue( const AValue : String; ACanvas : TCanvas; const ARect : TRect; ASelected : Boolean );
 
   public
     procedure Edit(); override;
     function  GetAttributes() : TPropertyAttributes; override;
-    procedure GetValues( Proc : TGetStrProc ); override;
+    procedure GetValues( AProc : TGetStrProc ); override;
     procedure Initialize(); override;
     function  GetName(): String; override;
     function  GetValue() : String; override;
     function  GetValueW() : WideString; override;
-    procedure SetValue(const Value: String); overload; override;
-    procedure SetValue(const Value: WideString); overload; override;
-    function  GetParentString() : String;
-    procedure SetParentString( AValue : String );
+    procedure SetValue( const AValue : String); overload; override;
+    procedure SetValue( const AValue : WideString); overload; override;
+    function  GetParentStrings() : IStringArrayList;
+    procedure SetParentString( const AValue : String );
     function  ValueAvailable() : Boolean;
-    procedure GetProperties(Proc: TGetPropProc); override;
+    procedure GetProperties( AProc : TGetPropProc ); override;
     function  GetPropCount() : Integer;
     function  GetDesigner() : DesignIntf.IDesigner;
 
@@ -82,30 +82,31 @@ end;
 //---------------------------------------------------------------------------
 function TOWPinListOwnerPropertyEditorProxy.GetValue() : String;
 begin
-  Result := FNestedEditor.GetStringValue();
+  Result := FNestedEditor.GetStringValues()[ 0 ];
 end;
 //---------------------------------------------------------------------------
 function TOWPinListOwnerPropertyEditorProxy.GetValueW() : WideString;
 begin
-  Result := FNestedEditor.GetStringValue();
+  Result := FNestedEditor.GetStringValues()[ 0 ];
 end;
 //---------------------------------------------------------------------------
-procedure TOWPinListOwnerPropertyEditorProxy.SetValue(const Value: String);
+procedure TOWPinListOwnerPropertyEditorProxy.SetValue( const AValue : String);
 begin
   FNestedEditor.SetStringValue( Value );
 end;
 //---------------------------------------------------------------------------
-procedure TOWPinListOwnerPropertyEditorProxy.SetValue(const Value: WideString);
+procedure TOWPinListOwnerPropertyEditorProxy.SetValue( const AValue : WideString);
 begin
   FNestedEditor.SetStringValue( Value );
 end;
 //---------------------------------------------------------------------------
-function TOWPinListOwnerPropertyEditorProxy.GetParentString() : String;
+function TOWPinListOwnerPropertyEditorProxy.GetParentStrings() : IStringArrayList;
 begin
-  Result := inherited GetValue();
+  Result := TStringArrayList.Create();
+  Result.Add( inherited GetValue());
 end;
 //---------------------------------------------------------------------------
-procedure TOWPinListOwnerPropertyEditorProxy.SetParentString( AValue : String );
+procedure TOWPinListOwnerPropertyEditorProxy.SetParentString( const AValue : String );
 begin
   inherited SetValue( AValue );
 end;
@@ -118,9 +119,9 @@ begin
 
 end;
 //---------------------------------------------------------------------------
-procedure TOWPinListOwnerPropertyEditorProxy.GetProperties( Proc: TGetPropProc );
+procedure TOWPinListOwnerPropertyEditorProxy.GetProperties( AProc : TGetPropProc );
 begin
-  if( EditorGetProperties( FNestedEditor, Designer, Proc )) then
+  if( EditorGetProperties( FNestedEditor, Designer, AProc )) then
     Exit;
 
   inherited;
@@ -136,7 +137,7 @@ begin
   Result := Designer;
 end;
 //---------------------------------------------------------------------------
-function TOWPinListOwnerPropertyEditorProxy.QueryInterface(const IID: TGUID; out Obj): HResult; stdcall;
+function TOWPinListOwnerPropertyEditorProxy.QueryInterface( const IID: TGUID; out Obj): HResult; stdcall;
 begin
   Result := EditorQueryInterface( FNestedEditor, IID, Obj);
   if( Succeeded( Result ) ) then
@@ -144,38 +145,38 @@ begin
 
 end;
 //---------------------------------------------------------------------------
-procedure TOWPinListOwnerPropertyEditorProxy.PropDrawName(ACanvas: TCanvas; const ARect: TRect; ASelected: Boolean);
+procedure TOWPinListOwnerPropertyEditorProxy.PropDrawName( ACanvas : TCanvas; const ARect : TRect; ASelected : Boolean );
 begin
   EditorPropDrawName( Self, FNestedEditor, ACanvas, ARect, ASelected );
 end;
 //---------------------------------------------------------------------------
-procedure TOWPinListOwnerPropertyEditorProxy.PropDrawValue(ACanvas: TCanvas; const ARect: TRect; ASelected: Boolean);
+procedure TOWPinListOwnerPropertyEditorProxy.PropDrawValue( ACanvas : TCanvas; const ARect : TRect; ASelected : Boolean );
 begin
   EditorPropDrawValue( Self, FNestedEditor, ACanvas, ARect, FValueRect, ASelected );
 end;
 //---------------------------------------------------------------------------
-function TOWPinListOwnerPropertyEditorProxy.PropDrawNameRect(const ARect: TRect): TRect;
+function TOWPinListOwnerPropertyEditorProxy.PropDrawNameRect( const ARect : TRect ) : TRect;
 begin
   Result := EditorPropDrawNameRect( FNestedEditor, ARect );
 end;
 //---------------------------------------------------------------------------
-function TOWPinListOwnerPropertyEditorProxy.PropDrawValueRect(const ARect: TRect): TRect;
+function TOWPinListOwnerPropertyEditorProxy.PropDrawValueRect( const ARect : TRect ) : TRect;
 begin
   FValueRect := ARect;
   Result := EditorPropDrawValueRect( FNestedEditor, ARect );
 end;
 //---------------------------------------------------------------------------
-procedure TOWPinListOwnerPropertyEditorProxy.ListMeasureWidth(const Value: String; ACanvas: TCanvas; var AWidth: Integer);
+procedure TOWPinListOwnerPropertyEditorProxy.ListMeasureWidth( const AValue : String; ACanvas : TCanvas; var AWidth: Integer );
 begin
   EditorListMeasureWidth( FNestedEditor, Value, ACanvas, AWidth );
 end;
 //---------------------------------------------------------------------------
-procedure TOWPinListOwnerPropertyEditorProxy.ListMeasureHeight(const Value: String; ACanvas: TCanvas; var AHeight: Integer);
+procedure TOWPinListOwnerPropertyEditorProxy.ListMeasureHeight( const AValue : String; ACanvas : TCanvas; var AHeight : Integer );
 begin
   EditorListMeasureHeight( FNestedEditor, Value, ACanvas, AHeight );
 end;
 //---------------------------------------------------------------------------
-procedure TOWPinListOwnerPropertyEditorProxy.ListDrawValue(const Value: String; ACanvas: TCanvas; const ARect: TRect; ASelected: Boolean);
+procedure TOWPinListOwnerPropertyEditorProxy.ListDrawValue( const AValue : String; ACanvas : TCanvas; const ARect : TRect; ASelected : Boolean );
 begin
   EditorListDrawValue( FNestedEditor, Value, ACanvas, ARect, ASelected );
 end;
@@ -190,15 +191,16 @@ end;
 //---------------------------------------------------------------------------
 function TOWPinListOwnerPropertyEditorProxy.GetAttributes() : TPropertyAttributes;
 begin
-  Result := inherited + CheckEditorAttributes( FNestedEditor );
+  Result := inherited;
+  CheckEditorAttributes( FNestedEditor, Result );
 end;
 //---------------------------------------------------------------------------
-procedure TOWPinListOwnerPropertyEditorProxy.GetValues( Proc : TGetStrProc );
+procedure TOWPinListOwnerPropertyEditorProxy.GetValues( AProc : TGetStrProc );
 begin
   if( not HInGetValues ) then
     begin
     HInGetValues := True;
-    if( CheckEditorDropDownList( FNestedEditor, Proc )) then
+    if( CheckEditorDropDownList( FNestedEditor, AProc )) then
       begin
       HInGetValues := False;
       Exit;
